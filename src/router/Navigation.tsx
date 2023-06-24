d8419c6a5fc455f2e036811278b79785e02eb009
Oliver Coimbra <olivercoimbra@gmail.com>
2023-06-24 17:00:41 -0400

    Add SideMenu and Menu Options

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,16 +1,20 @@
 import {FC} from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { LoginPage, DashboardPage, RegisterPage } from '../pages'
+import { MainLayout } from '../layouts/MainLayout'
 
 export const Navigation:FC = () => {
   return (
     <Routes>
       {/* RUTAS DE LA AUTENTICACION */}
+
       <Route path='/auth/login' element= { <LoginPage/> }/>
       <Route path='/auth/register' element= { <RegisterPage/> }/>
 
       {/* RUTAS PROTEGIDAS */}
-      <Route path='/' element= { <DashboardPage/> }/>
+      <Route path='/' element={ <MainLayout/>}>
+        <Route index element= { <DashboardPage/> }/>
+      </Route>
       
     </Routes>
   )
