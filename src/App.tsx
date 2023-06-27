17b634eb0ff6cfe6a03291e192337355def2d014
Oliver Coimbra <olivercoimbra@gmail.com>
2023-06-27 18:16:07 -0400

    Supabase conection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import {BrowserRouter} from "react-router-dom"
 import { Navigation } from "./router/Navigation"
+import { supabase } from "./supabase"
 
 
+console.log(supabase)
 
 const App = () => {
   return (
    <BrowserRouter>
     <Navigation/>
-   
    </BrowserRouter>
   )
 }
